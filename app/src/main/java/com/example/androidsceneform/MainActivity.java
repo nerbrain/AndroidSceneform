246d5825e77a04e14a83751ab9071c85a55293ab
nerbrain <malikmox@gmail.com>
2020-02-04 14:50:05 +0300

    Created OnClickListner for plan to be filled in with objects

--- app/src/main/java/com/example/androidsceneform/MainActivity.java
+++ app/src/main/java/com/example/androidsceneform/MainActivity.java
@@ -2,12 +2,116 @@ package com.example.androidsceneform;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Transformation;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArFragment arFragment;
+    private ModelRenderable bearRenderable,
+                            catRenderable,
+                            cowRenderable,
+                            elephantRenderable,
+                            ferretRenderable,
+                            hippopotamusRenderable,
+                            horseRenderable,
+                            koala_bearRenderable,
+                            lionRenderable,
+                            reindeerRenderable,
+                            wolverineRenderable;
+
+    ImageView bear,cow,dog,elephant,ferret,hippo,horse,koala,lion,reindeer,wolverine;
+
+    View arrayView[];
+    ViewRenderable name_animal;
+
+    int selected = 1; // Default Bear is selected
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.sceneform_ux_fragment);
+
+        //View
+        bear = (ImageView)findViewById(R.id.bear);
+        cow = (ImageView)findViewById(R.id.cow);
+        dog = (ImageView)findViewById(R.id.dog);
+        elephant = (ImageView)findViewById(R.id.elephant);
+        ferret = (ImageView)findViewById(R.id.ferret);
+        hippo = (ImageView)findViewById(R.id.hippopotamus);
+        horse = (ImageView)findViewById(R.id.horse);
+        koala = (ImageView)findViewById(R.id.koala_bear);
+        lion = (ImageView)findViewById(R.id.lion);
+        reindeer = (ImageView)findViewById(R.id.reindeer);
+        wolverine = (ImageView)findViewById(R.id.wolverine);
+
+        setArrayView();
+        setClickListener();
+
+        setupModel();
+
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                //When user taps on a plane surface, a model will be added
+
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    createModel(anchorNode, selected);
+            }
+        });
+    }
+
+    private void setupModel() {
+        ModelRenderable.builder()
+                .setSource(this,R.id.bear)
+                .build().thenAccept(renderable -> bearRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load bear Model", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                        );
+    }
+
+    private void createModel(AnchorNode anchorNode, int selected) {
+        if (selected ==1 ){
+            TransformableNode bear = new TransformableNode(arFragment.getTransformationSystem());
+            bear.setParent(anchorNode);
+            bear.setRenderable(bearRenderable);
+            bear.select();
+
+        }
+    }
+
+    private void setClickListener() {
+        for (int i=0; i<arrayView.length;i++){
+            arrayView[i].setOnClickListener((View.OnClickListener) this);
+        }
+    }
+
+    private void setArrayView() {
+        arrayView = new View []{
+                bear,cow,dog,elephant,ferret,hippo,horse,koala,lion,reindeer,wolverine
+        };
     }
 }
